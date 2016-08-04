76f5ca464ea013116661a599f778d67757d18dc9
Jeff Walker <walker.jeff.d@gmail.com>
2016-08-04 08:07:53 -0400

    initial commit
    - set up project template
    - load data from db
    - initial eda rmd

---
+++ main.R
@@ -0,0 +1,8 @@
+# Malden River Water Quality Data Analysis
+
+
+# load data ---------------------------------------------------------------
+source("load-wq-db.R")  # Windows only, R in 32-bit mode
+source("load-wq-csv.R")
+
+
---
+++ packages.R
@@ -0,0 +1,12 @@
+library(yaml)
+library(myrwaR)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(readr)
+library(ggplot2)
+
+theme_set(theme_bw())
+theme_update(strip.background = element_blank(),
+             strip.text = element_text(face = "bold"))
+
